6b24310bd543d5486d19b10a56341bc9c09c3bb0
thxforall <113906780+thxforall@users.noreply.github.com>
2025-05-20 22:37:32 +0900

    feat: favicon update

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -6,12 +6,17 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon/favicon.ico' },
-      { url: '/favicon/icon1.png', type: 'image/png' },
-      { url: '/images/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/images/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/favicon/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/favicon/favicon-48x48.png', type: 'image/png', sizes: '48x48' },
+      { url: '/favicon/favicon-64x64.png', type: 'image/png', sizes: '64x64' },
+      { url: '/favicon/favicon-128x128.png', type: 'image/png', sizes: '128x128' },
+      { url: '/favicon/favicon-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/favicon/favicon-256x256.png', type: 'image/png', sizes: '256x256' },
+      { url: '/favicon/favicon-512x512.png', type: 'image/png', sizes: '512x512' },
     ],
     apple: [
-      { url: '/favicon/apple-icon.png', type: 'image/png' },
+      { url: '/favicon/favicon-192x192.png', type: 'image/png' },
     ],
     other: [
       { url: '/favicon/icon0.svg', type: 'image/svg+xml' },
